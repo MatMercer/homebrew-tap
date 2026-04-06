98700728f9a2618b998539fe930f67f6d47b825e
Mateus Mercer <mateusmercer@gmail.com>
2026-04-06 17:41:21 -0300

    Update mactic to v2.0

--- mactic.rb
+++ mactic.rb
@@ -1,8 +1,9 @@
 class Mactic < Formula
   desc "Send haptic waveforms and visualize multitouch input on MacBook trackpads"
   homepage "https://github.com/MatMercer/mactic"
-  url "https://github.com/MatMercer/mactic/releases/download/v1.2/mactic-macos.tar.gz"
-  sha256 "4497f43db75376a957b60277869908ca6d6521b37d3bd6bf4b14040a3dd32f10"
+  url "https://github.com/MatMercer/mactic/releases/download/v2.0/mactic-macos.tar.gz"
+  version "2.0"
+  sha256 "e30c7522c9e526fb22bbe693aecf1a51e7ee651f5f7d87f03058627fd312f85f"
   license "Unlicense"
 
   depends_on :macos
