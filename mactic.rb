086c2eead09896c20c29795a1a2baf2f20a48718
Mateus Mercer <mateusmercer@gmail.com>
2026-04-06 17:06:24 -0300

    mactic v1.2: pre-built binary

--- mactic.rb
+++ mactic.rb
@@ -1,14 +1,13 @@
 class Mactic < Formula
   desc "Send haptic waveforms and visualize multitouch input on MacBook trackpads"
   homepage "https://github.com/MatMercer/mactic"
-  url "https://github.com/MatMercer/mactic/archive/refs/tags/v1.tar.gz"
-  sha256 "b506ef261ad777fffc1e63bef32bce5f38e7397aa34328776c860b8b3a3a8e3d"
+  url "https://github.com/MatMercer/mactic/releases/download/v1.2/mactic-macos.tar.gz"
+  sha256 "4497f43db75376a957b60277869908ca6d6521b37d3bd6bf4b14040a3dd32f10"
   license "Unlicense"
 
   depends_on :macos
 
   def install
-    system "make"
     bin.install "mactic"
   end
 
